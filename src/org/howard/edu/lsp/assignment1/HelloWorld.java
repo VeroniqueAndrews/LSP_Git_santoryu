1fe310c073e21b4f6b9dae5be0514c662b9b8392
Veronique Andrews <98068655+VeroniqueAndrews@users.noreply.github.com>
2023-09-20 16:18:27 -0400

    Add files via upload

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	/**
+	 * Veronique Andrews
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
